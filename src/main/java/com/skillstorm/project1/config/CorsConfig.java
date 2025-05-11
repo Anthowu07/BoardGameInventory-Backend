442e9097548b0226a1748607e580fd8cd0443eb0
Anthowu07 <anthony07wu@gmail.com>
2025-05-11 00:06:49 -0400

    reset

--- src/main/java/com/skillstorm/project1/config/CorsConfig.java
+++ src/main/java/com/skillstorm/project1/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all paths
-                        .allowedOrigins("https://boardgameinventory-backend-production.up.railway.app") // Allow specific origin
+                        .allowedOrigins("https://boardgameinventory-backend-production.up.railway.app", "https://anthowu07.github.io/") // Allow specific origin
                         //.allowedOrigins("http://localhost:5173/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
                         .allowedHeaders("*") // Allow all headers
